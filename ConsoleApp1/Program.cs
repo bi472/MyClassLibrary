7be2739fd938313c4d035115155478a140262673
Aidar <matt.nolimiteron@gmail.com>
2023-05-26 21:00:17 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using MyClassLibrary;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "input.txt";
+            string resultFilePath = "output.txt";
+
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine($"Input file '{filePath}' not found.");
+                return;
+            }
+
+            // Чтение текста из файла
+            string text = File.ReadAllText(filePath);
+
+            // Создание экземпляра класса из DLL
+            var classInstance = new Class1();
+            var type = typeof(Class1);
+            var method = type.GetMethod("ProcessText", BindingFlags.NonPublic | BindingFlags.Instance);
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            // Вызов приватного метода из DLL
+            Dictionary<string, int> result = (Dictionary<string, int>)method.Invoke(classInstance, new object[] { text }); ;
+            stopwatch.Stop();
+            Console.WriteLine("Время выполнения приватного метода: " + stopwatch.Elapsed);
+
+            stopwatch.Restart();
+            // Вызов публичного метода, реализующий обработку текста используя потоки
+            Dictionary<string, int> parallelResult = classInstance.ProcessTextParallel(text);
+            stopwatch.Stop();
+            Console.WriteLine("Время выполнения публичного метода с многопоточной обработкой: " + stopwatch.Elapsed);
+
+            // Запись результата в файл
+            using (StreamWriter writer = new StreamWriter(resultFilePath))
+            {
+                foreach (var entry in parallelResult)
+                {
+                    writer.WriteLine($"{entry.Key}: {entry.Value}");
+                }
+            }
+
+            Console.WriteLine("Обработка текста завершена. Результат записан в файл.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MyClassLibrary/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLibrary
+{
+    public class Class1
+    {
+
+        public Dictionary<string, int> ProcessTextParallel(string text)
+        {
+            // Разбиение текста на отдельные части
+            string[] parts = text.Split(new char[] { ' ', ',', '.', ':', ';', '(', ')', '[', ']', '-', '!', '?', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries); ;
+
+            // Создание результирующего словаря
+            Dictionary<string, int> result = new Dictionary<string, int>();
+
+            // Запуск обработки каждой части текста в отдельном потоке
+            Parallel.ForEach(parts, part =>
+            {
+                // Обработка текущей части текста
+                Dictionary<string, int> partialResult = ProcessText(part);
+
+                // Добавление результатов в результирующий словарь
+                lock (result)
+                {
+                    foreach (var entry in partialResult)
+                    {
+                        if (result.ContainsKey(entry.Key))
+                        {
+                            result[entry.Key] += entry.Value;
+                        }
+                        else
+                        {
+                            result[entry.Key] = entry.Value;
+                        }
+                    }
+                }
+            });
+
+            return result;
+        }
+
+
+        private Dictionary<string, int> ProcessText(string text)
+        {
+            Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+
+            string[] words = text.Split(new char[] { ' ', ',', '.', ':', ';', '(', ')', '[', ']', '-', '!', '?', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string word in words)
+            {
+                string cleanWord = word.ToLowerInvariant();
+                if (!wordCounts.ContainsKey(cleanWord))
+                {
+                    wordCounts[cleanWord] = 0;
+                }
+                wordCounts[cleanWord]++;
+            }
+
+            return wordCounts;
+        }
+    }
+}
